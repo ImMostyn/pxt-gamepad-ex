461863ad4ddfa88dcd2e92e04af9fe7eb5b8894a
Mostyn Sherlock <mostyn@hotmail.com>
2026-03-16 09:22:16 +0000

    Fixed test format for binary to string convertion

--- main.ts
+++ main.ts
@@ -1,13 +1,12 @@
-function toBinary32 (n: number) : string {
-    let result = ""
+function toBinary32 (n: number) {
+    result = ""
+    n = n >>> 0;
+for (let i = 31; i >= 0; i--) {
+        const bit = ((n >>> i) & 1) ? "1" : "0";
+        result += bit;
 
-    n = n >>> 0
-    
-    for (let i = 31; i >= 0; i--) {
-        let bit = ((n >> i) & 1) > 0 ? "1" : "0" 
-        result = result + bit
-        if (i % 8 == 0 && i != 0) {
-            result = result + "-"
+        if (i % 8 === 0 && i !== 0) {
+            result += "-";
         }
     }
 return result
@@ -16,21 +15,24 @@ input.onButtonPressed(Button.AB, function () {
     isLogging = !(isLogging)
 })
 let isLogging = false
+let result3 = ""
+let result2 = ""
 let result = ""
 serial.redirectToUSB()
 Gamepadex.startBroadcast(1, Frequencies.TwoFiftyHz)
 Gamepadex.stopBroadcast()
 basic.forever(function () {
     if (isLogging) {
-        serial.writeLine(" " + (toBinary32(Gamepadex.getGamepadState() & (ComponentMasks.Buttons | ComponentMasks.Orientation))))
+        serial.writeNumber(Gamepadex.joystickY())
+        serial.writeLine("" + (toBinary32(Gamepadex.getGamepadState() & (ComponentMasks.Buttons | ComponentMasks.Orientation))))
         serial.writeValue("Buttons: ", Gamepadex.getGamepadState() & ComponentMasks.Buttons)
         serial.writeValue("Stick X: ", (Gamepadex.getGamepadState() & ComponentMasks.HorizontalStick) >>> 8)
         serial.writeValue("Stick Y: ", (Gamepadex.getGamepadState() & ComponentMasks.VerticalStick) >>> 16)
         serial.writeValue("Orientation: ", (Gamepadex.getGamepadState() & ComponentMasks.Orientation) >>> 24)
-        if (Gamepadex.isOrientated(GestureFlags.Shake)) {
-            radio.sendValue("name", 0)
-            radio.sendNumber(0)
-        }
-        basic.pause(100)
     }
+    if (Gamepadex.isOrientated(GestureFlags.Shake)) {
+        radio.sendValue("name", 0)
+        radio.sendNumber(0)
+    }
+    basic.pause(100)
 })
